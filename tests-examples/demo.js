3dbb5084438f9aa54aac0f54fe4c36fa9e30b870
AmbarishDash15 <122718248+AmbarishDash15@users.noreply.github.com>
2025-12-25 15:00:14 +1100

    typescript installation

---
+++ tests-examples/demo.js
@@ -0,0 +1,4 @@
+var message = 'Jai Shree Ganesh';
+var count = 108;
+console.log(message);
+console.log(count);
---
+++ tests-examples/demo.ts
@@ -0,0 +1,4 @@
+var message : string = 'Jai Shree Ganesh';
+var count : number = 108;
+console.log(message);
+console.log(count);
